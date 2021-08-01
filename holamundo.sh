313f9586b0943fa797a921b6338709d14c42e5fb
danobook <63825822+danobook@users.noreply.github.com>
2021-08-01 15:57:57 -0500

    Create holamundo.sh
    mi primer hola mundo en bash

---
+++ holamundo.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#Todos los escrip en bash deben comenzar con la linea de arriba
+# esto indica al compilador que se trata de un archivo bash...  
+#Esto es un comentario  comienza por el  signo gato o numeral
+
+# Para imprimir algo en pantalla se usa el comando echo 
+
+echo hola mundo
+
+#Este es mi primer programa hola mundo en bash y lo voy a guardar en github
+
